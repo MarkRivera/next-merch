2942d5a95ee47718cb05227f7373cb70d4e44f62
Mark Rivera <Mrivera1991@gmail.com>
2021-09-20 15:04:28 -0700

    finished landing page

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,29 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
-import Layout from "../components/layout";
+import merchLogo from "../public/images/bg-text.webp";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Mike Merchant</title>
+        <title>Mike v Merchant | Mikemerch</title>
         <meta name="description" content="Mike" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Layout>
-          <h1>Click a site to enter:</h1>
-          <ul>
-            <li>
-              <Link href="/mike/">
-                <a>Mike</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/merch/">
-                <a>Merch</a>
-              </Link>
-            </li>
-          </ul>
-        </Layout>
+      <main className={styles.landingContainer}>
+        <span>Click a site to enter</span>
+        <nav className={styles.landingNav}>
+          <Link href="/mike/" className={styles.landingLink}>
+            <a className={styles.landingMike}>Mike</a>
+          </Link>
+          <div className={styles.landingDivider} />
+          <Link href="/merch/" className={styles.landingLink}>
+            <a className={styles.landingMerch}>Merch</a>
+          </Link>
+        </nav>
       </main>
-    </div>
+    </>
   );
 }
